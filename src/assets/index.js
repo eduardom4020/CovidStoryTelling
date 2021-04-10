1d266620ee59ffa01de306a8ce3710533e522707
Eduardo Melo <eduardo.melo.braga@gmail.com>
2021-04-10 13:36:04 -0300

    feat - putted two slides

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
-import vaccinations from './vaccinations_2021-04-03.json';
-import jhuFullData from './jhu_full_data_2021-04-03.json';
+import vaccinations from './vaccinations_2021-04-07.json';
+import jhuFullData from './jhu_full_data_2021-04-07.json';
 import covidLatest from './covid_latest_data_2021-04-07.json';
 import susCovidLatest from './sus_covid_latest_data_2021-04-07.json';
 
