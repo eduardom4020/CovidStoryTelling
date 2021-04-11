666cb18a50a9b392ca8050d32c4048016a51a052
Eduardo Melo <eduardo.melo.braga@gmail.com>
2021-04-11 16:03:26 -0300

    feat - finished slide 1

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,5 +12,7 @@ export const POPULATION_BRAZIL = 211000000;
 export const POPULATION_UNITED_KINGDOM = 66200000;
 
 export const ICON_VACCINATION = 'https://static.thenounproject.com/png/2970515-200.png';
+export const ICON_INFECTION = 'https://img.icons8.com/ios/452/coronavirus--v2.png';
+export const ICON_MOURNING = 'https://static.thenounproject.com/png/2940364-200.png';
 export const RESOURCE_NAME_VACCINATIONS_EUA = 'vaccinationsUSA';
 export const RESOURCE_NAME_JHU_FULL_DATA_EUA = 'jhuFullDataUSA';
